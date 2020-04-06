f215af6745b91dd9979ca85ae0ad411bea14bb11
ActuallySam <srivastava.samarth.btech2018@sitpune.edu.in>
2020-04-06 21:53:44 +0530

    2nd Commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -19,6 +19,7 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.SparseArray;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -30,8 +31,11 @@ import com.google.android.gms.vision.text.TextRecognizer;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
+//The Main Class
+//AppCompatActivity in the Video
 public class MainActivity extends AppCompatActivity {
 
+    //Initialize Variable for all the Button ID's you created in the xml file
     EditText mResultEt;
     ImageView mPreviewIv;
 
@@ -46,14 +50,21 @@ public class MainActivity extends AppCompatActivity {
 
     Uri image_uri;
 
+    //There's always an Override method in the Main Class
+    //@Override also explained in the video
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);  //The design layout xml file to which it will refer. Here, it refers to the "activity_main" xml file
 
+        //Assign Variables declared above to each and every Button by using findViewById...
+        //... to find the ID of the buttons from the xml file so that it links to the Java file...
+        //...and we can assign ant task that the particular tool will perform through the Java file
         mResultEt = findViewById(R.id.resultEt);
         mPreviewIv = findViewById(R.id.imageIv);
 
+        //Manifest file is used for all the permission that an app can require, like Internet access, camera and storage access
         //camera permission
         cameraPermission = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
         //storage permission
@@ -61,38 +72,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    //Actionbar Menu
+    //Actionbar Menu--->>>
+    //Refers to the 3-dots button which is used to pop a menu with certain options
+    //Located at the top right of the mobile screen
+    //OnCreateOptionsMenu() to specify the options menu for an activity. You can inflate your menu resource by this method
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        /*In (Menu menu) -->> Menu is just the type of the parameter menu. For example you can have a String type for a variable named string, dog, etc....
+    ...And in this case there's a Menu type for a parameter named menu, denoted by menu_main xml file
+        */
+
         //inflate menu
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);  // //The design layout xml file to which it will refer. Here, it refers to the "menu_main" xml file
         return true;
     }
 
-    //Handle Actionbar item clicks
+    //--------------------------------------------------------------------------
 
+
+    //Handle Actionbar item clicks--->>>
+
+    //The items that will be present in the ActionBar (or the top bar) is set here. onOptionsItemSelected() method is there to deal with this.
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        if(id == R.id.addImage){
-            showImageImportDialog();
+
+        int id = item.getItemId();  //To get the ID of the buttons that we declared in the "menu_main" xml file
+        if(id == R.id.addImage){   //Whenever the image button is clicked...
+            showImageImportDialog();   //...Jump to this function
         }
-        if(id == R.id.settings){
-            Toast.makeText(this,"Settings", Toast.LENGTH_SHORT).show();
+        if(id == R.id.settings){    //On clicking the settings option in the Menu Inflater...
+            Toast.makeText(this,"Settings", Toast.LENGTH_SHORT).show();  //Toast is a small bar that appears at the bottom of the mobile screen
         }
         return super.onOptionsItemSelected(item);
     }
 
+    //-------------------------------------------------------------------------------
+
+    //Access Commands --->>
+
     private void showImageImportDialog(){
         //items to display in dialog
+        //AlertDialog.Builder object is created to display the 2 options that will be displayed for the user, to choose between then
         String[] items = {"Camera", "Gallery"};
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        //set title
+        //setting title of the Dialog Box
         dialog.setTitle("Select Image");
         dialog.setItems(items, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                if(which == 0){
+                if(which == 0){  //If you've selected Camera
                     //camera option clicked
                     if(!checkCameraPermission()){
                         //camera permission not allowed, request it
@@ -103,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
                         pickCamera();
                     }
                 }
-                if(which == 1){
+                if(which == 1){  //If you've selected Gallery
                     //gallery option clicked
                     if(!checkStoragePermission()){
                         //storage permission not allowed, request it
@@ -119,6 +148,10 @@ public class MainActivity extends AppCompatActivity {
         dialog.create().show();   //show dialog
     }
 
+    //------------------------------------------------------------------
+
+
+    //Gallery ----->>>>>
     private void pickGallery() {
         //intent to pick the image from gallery
         Intent intent = new Intent(Intent.ACTION_PICK);
@@ -127,6 +160,10 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, IMAGE_PICK_GALLERY_CODE);
     }
 
+    //--------------------------------------------------------------------
+
+
+    //Camera ---------->>>>>
     private void pickCamera(){
         //intent to take the image from the camera, it will also be saved to the storage to get the high quality image
         ContentValues values = new ContentValues();
@@ -134,28 +171,49 @@ public class MainActivity extends AppCompatActivity {
         values.put(MediaStore.Images.Media.DESCRIPTION, "Image to Text"); //description
         image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
 
+        //The contract between the media provider and applications. Contains definitions for the supported URI's
+        //Standard Intent action that can be sent to have the camera application capture an image and return it.
+
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        //The name of the Intent-extra used to indicate a content resolver Uri to be used to store the requested image
         cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
         startActivityForResult(cameraIntent, IMAGE_PICK_CAMERA_CODE);
     }
 
+    //------------------------------------------------------------------
+
+
+    //Request Storage Permission ----->>>>
     private void requestStoragePermission() {
         ActivityCompat.requestPermissions(this, storagePermission, STORAGE_REQUEST_CODE);
 
     }
 
+    //------------------------------------------------------------------
+
+
+    //Check Storage Permission ---->>>
     private boolean checkStoragePermission() {
         boolean result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
         return result;
     }
 
+    //------------------------------------------------------------------
+
+
+    //Request Camera Permission ----->>>>
     private void requestCameraPermission() {
         ActivityCompat.requestPermissions(this, cameraPermission,CAMERA_REQUEST_CODE);
     }
 
+    //------------------------------------------------------------------
+
+
+    //Check Camera Permission ---->>>
     private boolean checkCameraPermission(){
         /* Check camera permission and return the result
-            *In order to get high quality image we havee to save the image to the external storage first
+            *In order to get high quality image we have to save the image to the external storage first
             * before inserting into image view , so the storage permission will also be required
          */
         boolean result = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == (PackageManager.PERMISSION_GRANTED);
@@ -163,22 +221,27 @@ public class MainActivity extends AppCompatActivity {
         return result & result1;
     }
 
+    //------------------------------------------------------------------
+
+
     //handle permission result
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch(requestCode){
             case CAMERA_REQUEST_CODE:
                 if(grantResults.length > 0){
+                    //Created a variable for Camera Permission and Storage Permission
                     boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
                     boolean writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
                     if(cameraAccepted && writeStorageAccepted){
                         pickCamera();
                     }
-                    else{
+                    else{  //Denying of permission of Camera and Storage
                         Toast.makeText(this, "permission denied", Toast.LENGTH_SHORT).show();
                     }
                 }
                 break;
+
             case STORAGE_REQUEST_CODE:
                 if(grantResults.length > 0){
                     boolean writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
@@ -186,24 +249,26 @@ public class MainActivity extends AppCompatActivity {
                         pickGallery();
                     }
                     else{
-                        Toast.makeText(this, "permisiion denied", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(this, "permission denied", Toast.LENGTH_SHORT).show();
                     }
                 }
                 break;
         }
     }
 
+    //-----------------------------------------------------------------------------------
+
 
     //handle image result
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if(resultCode == RESULT_OK){
-            if(requestCode == IMAGE_PICK_GALLERY_CODE){
+            if(requestCode == IMAGE_PICK_GALLERY_CODE){ //Picked up the image from gallery
                 //got image from gallery, now crop it
                 CropImage.activity(data.getData()).setGuidelines(CropImageView.Guidelines.ON) //enable image guidelines
                 .start(this);
             }
-            if(requestCode == IMAGE_PICK_CAMERA_CODE){
+            if(requestCode == IMAGE_PICK_CAMERA_CODE){  //Shot an image on Camera
                 //got image from camera, now crop it
                 CropImage.activity(image_uri).setGuidelines(CropImageView.Guidelines.ON) //enable image guidelines
                 .start(this);
@@ -214,13 +279,22 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if(resultCode == RESULT_OK){
-                Uri resultUri = result.getUri(); //get image uri
+                Uri resultUri = result.getUri(); //get image uri(url)
                 //set image to image view
                 mPreviewIv.setImageURI(resultUri);
 
                 //get drawable bitmap for text recognition
+                //BitmapDrawable is related to the part where the cropped image will be shown on the screen, and the character recognition will be done from that cropped image
+                //Hence it is using mPrevieIv (the button variable that links to the ID of the ImageView in the "activity_main" xml file
+
                 BitmapDrawable bitmapDrawable = (BitmapDrawable)mPreviewIv.getDrawable();
                 Bitmap bitmap = bitmapDrawable.getBitmap();
+
+                //TextRecognizer is an API provided by Google specially for OCR apps
+                //recognizer object is created to support the working of the TextRecognizer API
+                /*getApplicationContext() - Return the context of the process of all activity running inside it. If you need a context that bind entire lifecycle of application you can use this. You should use it in across the activity.
+                Example Use: If you have to create a singleton object for your application and that object needs a context, always pass the application context.
+                If you pass the activity context here, it will lead to the memory leak as it will keep the reference to the activity and activity will not be garbage collected. */
 
                 TextRecognizer recognizer = new TextRecognizer.Builder(getApplicationContext()).build();
 
@@ -228,8 +302,16 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
                 }
                 else{
+                    //TextRecognizer - Finds and recognizes text in a supplied Frame.
+                    //Object created for the Frame which will bounce through each word, considering each word as a separate frame and each space between the words are for breaking one frame from another
+
                     Frame frame = new Frame.Builder().setBitmap(bitmap).build();
-                    SparseArray<TextBlock> items = recognizer.detect(frame);
+                    SparseArray<TextBlock> items = recognizer.detect(frame); //Finds and recognizes text in a supplied Frame.
+
+                    /*The principal operations on a StringBuilder are the append and insert methods, which are overloaded so as to accept data of any type.
+                     Each effectively converts a given datum to a string and then appends or inserts the characters of that string to the string builder.
+                      The append method always adds these characters at the end of the builder; the insert method adds the characters at a specified point.*/
+
                     StringBuilder sb = new StringBuilder();
                     //get text from sb until there is no text
                     for(int i = 0; i<items.size(); i++){
@@ -238,6 +320,7 @@ public class MainActivity extends AppCompatActivity {
                         sb.append("\n");
                     }
                     //set text to edit text
+                    //ToString() Returns a string representation of the object.
                     mResultEt.setText(sb.toString());
                 }
             }
